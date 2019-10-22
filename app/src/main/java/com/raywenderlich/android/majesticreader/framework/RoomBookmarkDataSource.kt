68cca292160c0377be0ab511fb6413d0e4c59e88
Omer Amiel <omeramiel@gmail.com>
2019-10-22 20:33:38 +0300

    cleanup

--- app/src/main/java/com/raywenderlich/android/majesticreader/framework/RoomBookmarkDataSource.kt
+++ app/src/main/java/com/raywenderlich/android/majesticreader/framework/RoomBookmarkDataSource.kt
@@ -4,15 +4,12 @@ import android.content.Context
 import com.raywenderlich.android.majesticreader.data.BookmarkDataSource
 import com.raywenderlich.android.majesticreader.domain.Bookmark
 import com.raywenderlich.android.majesticreader.domain.Document
-import com.raywenderlich.android.majesticreader.framework.db.BookmarkDao
 import com.raywenderlich.android.majesticreader.framework.db.BookmarkEntity
 import com.raywenderlich.android.majesticreader.framework.db.MajesticReaderDatabase
 
 class RoomBookmarkDataSource(context: Context) : BookmarkDataSource {
 
-    private val bookmarkDao by lazy { MajesticReaderDatabase.getInstance(context).bookmarkDao() }
-
-//    private val bookmarkDao = MajesticReaderDatabase.getInstance(context).bookmarkDao()
+    private val bookmarkDao = MajesticReaderDatabase.getInstance(context).bookmarkDao()
 
     override suspend fun add(document: Document, bookmark: Bookmark) =
             bookmarkDao.addBookmark(BookmarkEntity(documentUri = document.url, page = bookmark.page))
